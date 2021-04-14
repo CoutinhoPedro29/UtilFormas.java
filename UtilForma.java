69a3cde3d079da0a59c9ccc94185e5a78017470a
CoutinhoPedro29 <57445134+CoutinhoPedro29@users.noreply.github.com>
2021-04-14 18:55:21 -0300

    Create UtilForma.java

---
+++ UtilForma.java
@@ -0,0 +1,12 @@
+package entidades;
+
+public class ler extends Forma
+{
+public Double getArea(){
+    
+    public circ(Double raio);
+ public quad(Double rai);
+System.out.println(rai);
+System.out.println(raio);
+}
+}
